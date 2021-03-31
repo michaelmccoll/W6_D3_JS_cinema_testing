ad2c7717f7f7090cfb599f3c367e8e427aa5279b
michaelmccoll <mccollmichael@hotmail.com>
2021-03-31 16:04:28 +0100

    Cinema labwork

---
+++ models/cinema.js
@@ -0,0 +1,41 @@
+const Cinema = function (films) {
+  this.films = films;
+};
+
+Cinema.prototype.filmTitles = function() {
+  return this.films.map((film)=>{ 
+    return film.title 
+  }) 
+}
+
+
+Cinema.prototype.findByTitle = function(title) {
+  return this.films.find((film)=>{
+    return film.title === title;
+  });
+}
+
+Cinema.prototype.findByGenre = function(genre) {
+  return this.films.filter((film)=>{
+    return film.genre === genre;
+  });
+}
+
+Cinema.prototype.findByYear = function(year) {
+  return this.films.filter((film)=>{
+    return film.year === year;
+  });
+}
+
+Cinema.prototype.checkLength = function(length) {
+  return this.films.filter((film)=>{
+    return film.length >= length;
+  });
+}
+
+Cinema.prototype.totalTime = function() {
+  this.films.reduce((runningTotal,length) => {
+    return runningTotal + length;
+},0);
+}
+module.exports = Cinema;
